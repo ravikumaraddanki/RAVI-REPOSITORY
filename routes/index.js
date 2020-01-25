3c6dd8857135ab803dd8d9f246868c8a1fbff19a
Ravi Kumar Addanki <ravia@ispace.com>
2020-01-25 11:33:22 +0530

    my-first-commit_App

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Azure' });
+  res.render('index', { title: 'Ravi-Azure' });
 });
 
 module.exports = router;
