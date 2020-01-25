5d905c3a830860d6f15d7a77068f5b23aa3ea455
Ravi Kumar Addanki <ravia@ispace.com>
2020-01-25 16:32:21 +0530

    my-first-commit3

--- app.js
+++ app.js
@@ -28,10 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 //new code
 app.use('/problem',function(){
@@ -39,14 +35,19 @@ app.use('/problem',function(){
 });
 //end
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-
+//new code
   appInsights.defaultClient.trackException({exception:err});
+  //end
   // render the error page
   res.status(err.status || 500);
   res.render('error');
